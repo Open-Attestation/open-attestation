1ba0450f69497c169745aa520a92f0288fbb8789
Nebulis <nebounet@gmail.com>
2020-08-18 13:44:32 +0800

    fix: build umd from esm output

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import commonjs from "rollup-plugin-commonjs";
 import json from "@rollup/plugin-json";
 
 export default {
-  input: "./dist/cjs/index.js", // umd will run from cjs build to produce a single bundled file, any better idea ?
+  input: "./dist/esm/index.js", // umd will run from esm build to produce a single bundled file, any better idea ?
   output: {
     file: "./dist/index.umd.js",
     format: "umd",
