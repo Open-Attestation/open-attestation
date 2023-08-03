9546b0d5738e2f787468c8f762b825a1ad9aa612
Kyle Huang Junyuan <kyle@hjunyuan.com>
2023-08-03 15:35:33 +0800

    fix: commitlint install issue and introduce typing

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,6 +1,10 @@
-module.exports = {
+import type { UserConfig } from "@commitlint/types";
+
+const Configuration: UserConfig = {
   extends: ["@commitlint/config-conventional"],
 
   // Add your own rules. See http://marionebl.github.io/commitlint
   rules: {},
 };
+
+module.exports = Configuration;
