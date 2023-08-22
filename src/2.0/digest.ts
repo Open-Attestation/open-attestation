739a574c94bf58d8fa7b66a5d1daf27e4860b9dc
Sim Boon Long <simboonlong@gmail.com>
2023-08-22 12:47:28 +0800

    fix: nope

--- src/2.0/digest.ts
+++ src/2.0/digest.ts
@@ -19,14 +19,14 @@ export const digestDocument = <T extends OpenAttestationDocument = OpenAttestati
   document: SchematisedDocument<T>
 ) => {
   // Prepare array of hashes from filtered data
-  const hashedDataArray = get(document, "privacy.obfuscatedData", []);
+  const hashedDataArray: string[] = get(document, "privacy.obfuscatedData", []);
 
   // Prepare array of hashes from visible data
   const unhashedData = get(document, "data");
   const hashedUnhashedDataArray = flattenHashArray(unhashedData);
 
   // Combine both array and sort them to ensure determinism
-  const combinedHashes = [...hashedDataArray, ...hashedUnhashedDataArray];
+  const combinedHashes = hashedDataArray.concat(hashedUnhashedDataArray);
   const sortedHashes = sortBy(combinedHashes);
 
   // Finally, return the digest of the entire set of data
