f2a87bf053efb14bf1de53a4be878de636efad07
Kyle Huang Junyuan <kyle@hjunyuan.com>
2023-08-31 15:49:33 +0800

    test: update fixtures

--- src/4.0/__tests__/digest.test.ts
+++ src/4.0/__tests__/digest.test.ts
@@ -7,7 +7,7 @@ import sample from "../../../test/fixtures/v4/did-wrapped.json";
 
 const verifiableCredential = sample as WrappedDocument;
 // Digest will change whenever sample document is regenerated
-const credentialRoot = "98d0979858c8d96c11373462b84e485863c7cec7a3c6b81f80c2673144839d48";
+const credentialRoot = "04a551b401900c95ac92056fc0bbc2d9300890d7adb9410af9c065c8be6338b3";
 
 const { proof, ...credential } = verifiableCredential;
 
@@ -29,7 +29,7 @@ describe("digest v4.0", () => {
 
       expect(obfuscatedVerifiableCredential.proof.privacy.obfuscated).toMatchInlineSnapshot(`
         [
-          "83151d9c1f36da1f39e2adb42affefd66ee40c834d38f5a89b240db9e30ccf04",
+          "fc6a3e4ac9ab3a77da2eb8ab35d4f3f02aeb1463acdc0edc0082b1b301f25269",
         ]
       `);
       expect(obfuscatedVerifiableCredential.proof.privacy.obfuscated).toHaveLength(1);
@@ -49,9 +49,9 @@ describe("digest v4.0", () => {
 
       expect(obfuscatedVerifiableCredential.proof.privacy.obfuscated).toMatchInlineSnapshot(`
         [
-          "b3acf9662d89be51e61794d73e4d5c355afd474f1c08d0227929404990bd3fd4",
-          "3a58e681d26494a01f00d7679c2a7eb725a329dbf4990521cd231714cb986f32",
-          "5d415abceac2bbf21b2ee03e8b6fb56a08bcce65045919ce2e56f77eb9a295fc",
+          "892da892b97e173a8c3ca82eb4e6aeb472684efb2ae2d21f1e04cb90343dd5db",
+          "94f6cfc087aa9d4a4ba8ff12901924d0efc60023208e3f8f3a5aff6ad68ae416",
+          "4a8cfa9eafbf3e6de4e8b83def6a47adf8421432239d0f13c6df1a6b3f417d23",
         ]
       `);
       expect(obfuscatedVerifiableCredential.proof.privacy.obfuscated).toHaveLength(3);
@@ -71,37 +71,36 @@ describe("digest v4.0", () => {
       expect(obfuscatedVerifiableCredential).toStrictEqual({ proof: obfuscatedVerifiableCredential.proof });
       expect(obfuscatedVerifiableCredential.proof.privacy.obfuscated).toMatchInlineSnapshot(`
         [
-          "6bb0814c2d4da2b6e65bd7f309b137820f75a72309d4433b6a65654ca25eb9e4",
-          "3fe0b259f06f8db4c24f714869ff9b8a461c30db543865102fd9e21f9480e1ff",
-          "d1564ddab477b0f4038842b0c0f2ebde03bfcbc80c935297287097f78c299598",
-          "6d325245e343a13f578fb106fdea79ff5b468867ce40f0f9a55aefa156d0a566",
-          "b1fbc543d5346c308483db1178eac169a002a77bcbac3fa2e4166f0576759cfe",
-          "f4e63f855e703e4ecb4eb7b385df05aaf4805c323697393631cf83c62a7f585e",
-          "5d7cd395ad239a9bc58172c25d1b60de505c4e6c2db3a4ebbf6ee9b065e99112",
-          "83151d9c1f36da1f39e2adb42affefd66ee40c834d38f5a89b240db9e30ccf04",
-          "0d168dc997ced57b338b09d8ca9c00cbe20cb91b2da9489c372e18570b26ac2c",
-          "111f2faaf43644cc53378c107ea939aa7de5a6d0c34bc910f763ae0645e02ffb",
-          "ffa0c76626b3961350bdd18aabc98058e50b3d6ae4db0b741839f2e405e32a75",
-          "083fce57e36ad296344603de334a83d326f128b97f9f55c776140f91f6735eb0",
-          "91deffeed39119cee5200e10401623b54caf972e77efcf8efb93d9fbe6f47222",
-          "9ae8b2ce6231b2eb9b28ad811eaafb0844560d9639d95382ae030fc0d91ca649",
-          "7288a389668a20ebab0ea701d36d4a1f5dc125e996b875713705d673f3f7e30a",
-          "1d46cb9bdffab2643ce2bc0485027e78e825813453a08dba451b46442b9452ff",
-          "cbd3bb27af0714e508b8d83d3ab35e450e182a4815e9f8b93d6cb5b2a9c2676b",
-          "1ffd2bfe8cab5ca7a991a2d55503d33d32fb04e86bd3aee11a11a723fff6b211",
-          "a8885080f61fcfcd511c6ce3d539f945c3b7b211fe69f9e2b0408b835b74f6b7",
-          "b3acf9662d89be51e61794d73e4d5c355afd474f1c08d0227929404990bd3fd4",
-          "1a819a3039cfbf30870cf68ca567691d37437a5356a14355d2936bfddc8e07f9",
-          "3a58e681d26494a01f00d7679c2a7eb725a329dbf4990521cd231714cb986f32",
-          "d56b1b93f20c0d204cc609ad0726363ee520da7ee3f0446f304f9fce2c1fbaa8",
-          "5d415abceac2bbf21b2ee03e8b6fb56a08bcce65045919ce2e56f77eb9a295fc",
-          "04d91d673245a38bdf50638fb34905d859eb13aa21c169c90742d3900d1a58a6",
-          "df89e72363a326993ff64894e824997e9cb0174d8309643a712448433556be5c",
-          "51e397c49c274a6e06ecbabf4572b4b932f894c093e67116109286a2c7e88b97",
-          "162930f89edeedec8502c642246771e3d03c61c9cb172ea7f85addce7a065372",
+          "b3fe78c46d1528e477aebafe4b674bfd2b10614f72b0224da8ee163ce1a2a4a9",
+          "02125acd8247c4bfce550fd6d3d5e9bf5a2672b97ba12f3f74f9c4d7b493673f",
+          "0499f1ff43d4e58e42bb47e01c58491e969e31a25b38e7c2c16fbb392a99b11d",
+          "d97f9e773179d20426fb89312ea955638ecf73e18bbf7e724cc13afeeaf5880d",
+          "d1d36975fd38cb0d7c250a8c0a482503a0d4e414947f56a7a07e0d73a902fb37",
+          "71677e56872a41cd2b62211a922082a5ecf0429e29e5cd78b0a72bf57d6fed46",
+          "176b76b75c642084ede156ebaf64b73db92ecba803ab58e702659ff987114c2c",
+          "fc6a3e4ac9ab3a77da2eb8ab35d4f3f02aeb1463acdc0edc0082b1b301f25269",
+          "d714614f162443162c5d1024e757485de7deea042667282910f75da80ade12cd",
+          "ad7fe8ff8c6f034f11d81a70e7c93d62f0db0c32f3162dad049ba769102a0005",
+          "dd7f22be800e9a335115a97e51674b9771bc287677119a2baca655d917f79bd4",
+          "4ca2aab7c5fa63d05559c47052e7a9729cd9179d0b0f05f2ecc82385d2734613",
+          "201c6f75473a3ca7e173f56d8377e7d3b790b7b6b2335b8110ca4002a6cd0db9",
+          "07e7c36c22f1ff86a13a23616d87614d2e68d6aac06f98acdea15e43de2bacb3",
+          "58be2327fcbb4f6489ac26108b26efeca27c37cde3f1df97e826644bed7a4d80",
+          "2134e615fa0390ec19a7d7776a983216a554af3edbd71f448b1642bc8d6d18ea",
+          "221e3f50bc552299c774fc45788c7abe3743bd39a9fc8797e26747aa983c404e",
+          "3614459068235a5b759f336c92cec7065b0631eace98a2b4a0b653ae8ecc02bc",
+          "892da892b97e173a8c3ca82eb4e6aeb472684efb2ae2d21f1e04cb90343dd5db",
+          "a6055dc88b52188e2da3bd08458d0e46930c9bcf2e42b65e49ff41f5ac960fe8",
+          "94f6cfc087aa9d4a4ba8ff12901924d0efc60023208e3f8f3a5aff6ad68ae416",
+          "9129e18dce97e3631fed2beb3cc4edb891ea04db2c2fe319231df83cea050b59",
+          "4a8cfa9eafbf3e6de4e8b83def6a47adf8421432239d0f13c6df1a6b3f417d23",
+          "9c6406e19d08650d2064a0e7f90c427961c27ecdda99489ee5ada95cc77d15fa",
+          "62ddf412190fd48dc529a1df9834ae2f036ab6d5ea9d45a40948e5aa399a25fd",
+          "f16d53781fdcdff8e1a9ac3f2428aa72b9f80ec43b440049a789e691ed869c59",
+          "54ac572f4cd33f77a4bcb647b4445e4434b7b6094a73d8b654d1a2831ebbe4f7",
         ]
       `);
-      expect(obfuscatedVerifiableCredential.proof.privacy.obfuscated).toHaveLength(28);
+      expect(obfuscatedVerifiableCredential.proof.privacy.obfuscated).toHaveLength(27);
       expect(digest).toBe(credentialRoot);
     });
   });
