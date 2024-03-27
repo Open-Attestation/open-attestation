75aec4ada52fb1c54922c17c5cc1ac38e7449f8c
Kyle Huang Junyuan <kyle@hjunyuan.com>
2024-03-27 17:58:02 +0800

    test: no more auto-correct for @context based on vc 2.0 test suite
    expected behaviour is to throw error

--- src/4.0/schema/schema.test.ts
+++ src/4.0/schema/schema.test.ts
@@ -25,51 +25,12 @@ describe("schema/4.0", () => {
   });
 
   describe("@context", () => {
-    it("should self-correct when @context is a string with a valid context, appending it to the array of @context", async () => {
-      // @context MUST be an ordered set in W3C VC data model, see https://www.w3.org/TR/vc-data-model/#contexts
-      const document = { ...cloneDeep(sampleVc), "@context": "https://w3id.org/traceability/v1" };
-      const wrappedDocument = await wrapDocumentV4(document as any);
-      expect(wrappedDocument["@context"]).toStrictEqual([
-        "https://www.w3.org/2018/credentials/v1",
-        ContextUrl.v4_alpha,
-        "https://w3id.org/traceability/v1",
-      ]);
-    });
-    it("should self-correct when @context is an array that contains a valid context but is missing the required contexts", async () => {
-      // This should not have AJV validation errors as it's only caught after
-      const document = { ...cloneDeep(sampleVc), "@context": ["https://w3id.org/traceability/v1"] };
-      const wrappedDocument = await wrapDocumentV4(document);
-      expect(wrappedDocument["@context"]).toStrictEqual([
-        "https://www.w3.org/2018/credentials/v1",
-        ContextUrl.v4_alpha,
-        "https://w3id.org/traceability/v1",
-      ]);
-    });
-    it("should self-correct when @context is an array that contains the required context but not the right order", async () => {
-      // This should not have AJV validation errors as it's only caught during validateW3C
-      const document = {
-        ...cloneDeep(sampleVc),
-        "@context": ["https://w3id.org/traceability/v1", "https://www.w3.org/2018/credentials/v1"],
-      };
-      const wrappedDocument = await wrapDocumentV4(document);
-      expect(wrappedDocument["@context"]).toStrictEqual([
-        "https://www.w3.org/2018/credentials/v1",
-        ContextUrl.v4_alpha,
-        "https://w3id.org/traceability/v1",
-      ]);
-    });
     it("should be invalid if @context contains one invalid URI", async () => {
       expect.assertions(1);
-      const document = { ...cloneDeep(sampleVc), "@context": ["https://www.w3.org/2018/credentials/v1", "any"] };
-      await expect(wrapDocumentV4(document)).rejects.toHaveProperty("validationErrors", [
-        {
-          keyword: "format",
-          instancePath: "/@context/2",
-          schemaPath: "#/properties/%40context/items/format",
-          params: { format: "uri" },
-          message: 'must match format "uri"',
-        },
-      ]);
+      const document = { ...cloneDeep(sampleVc), "@context": [ContextUrl.v2_vc, "bad string"] };
+      await expect(wrapDocumentV4(document)).rejects.toMatchInlineSnapshot(
+        `[Error: Unable to interpret @context: {"name":"jsonld.InvalidUrl","details":{"code":"loading remote context failed","url":"bad string","cause":{}}}]`
+      );
     });
   });
 });
