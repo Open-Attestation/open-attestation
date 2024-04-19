2ff1e948dcc6ecb7f160d627d8e3be6afb913b2c
Phan Shi Yu <phan_shi_yu@tech.gov.sg>
2024-04-19 18:34:44 +0800

    fix: should not allow obsfucating required fields

--- src/4.0/obfuscate.ts
+++ src/4.0/obfuscate.ts
@@ -5,10 +5,31 @@ import { cloneDeep, get, unset, pick } from "lodash";
 import { decodeSalt, encodeSalt } from "./salt";
 import { traverseAndFlatten } from "./traverseAndFlatten";
 
+type Temp<T> = {
+  [K in keyof T]: T extends Record<K, T[K]> ? K : never;
+};
+type KeysOfRequiredFields<T> = Temp<T> extends { [K in keyof T]: infer U } ? U : never;
+const REQUIRED: Record<KeysOfRequiredFields<OpenAttestationDocument>, true> = {
+  "@context": true,
+  type: true,
+  credentialSubject: true,
+  issuer: true,
+};
+const REQUIRED_FIELDS = Object.keys(REQUIRED);
+
+// RequiredMyType is now { requiredKey: string }
 const obfuscate = (_data: WrappedDocument<OpenAttestationDocument>, fields: string[] | string) => {
+  const fieldsAsArray = ([] as string[]).concat(fields);
+  for (const field of fieldsAsArray) {
+    for (const requiredField of REQUIRED_FIELDS) {
+      if (field.startsWith(requiredField)) {
+        throw new Error(`Field ${field} is required`);
+      }
+    }
+  }
+
   const data = cloneDeep(_data); // Prevents alteration of original data
 
-  const fieldsAsArray = ([] as string[]).concat(fields);
   // fields to remove will contain the list of each expanded keys from the fields passed in parameter, it's for instance useful in case of
   // object obfuscation, where the object itself is not part of the salts, but each individual keys are
   const fieldsToRemove: string[] = traverseAndFlatten(pick(data, fieldsAsArray), {
