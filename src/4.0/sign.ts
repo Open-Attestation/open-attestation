d39ceaa584ad026263b00cdd812411edf38ce637
Phan Shi Yu <phan_shi_yu@hive.gov.sg>
2024-04-25 01:52:51 +0800

    refactor: remove use of guard and remove use of algo enum

--- src/4.0/sign.ts
+++ src/4.0/sign.ts
@@ -1,20 +1,24 @@
 import { sign } from "../shared/signer";
-import { SigningKey, SUPPORTED_SIGNING_ALGORITHM } from "../shared/@types/sign";
+import { SigningKey } from "../shared/@types/sign";
 import { isSignedWrappedV4Document } from "../shared/utils";
 import { ethers } from "ethers";
 import { V4RawDocument, V4WrappedDocument, V4SignedDocument } from "./types";
 
 export const signDocument = async <T extends V4RawDocument>(
   document: V4SignedDocument<T> | V4WrappedDocument<T>,
-  algorithm: SUPPORTED_SIGNING_ALGORITHM,
+  algorithm: "Secp256k1VerificationKey2018",
   keyOrSigner: SigningKey | ethers.Signer
 ): Promise<V4SignedDocument<T>> => {
   if (isSignedWrappedV4Document(document)) throw new Error("Document has been signed");
+
   const merkleRoot = `0x${document.proof.merkleRoot}`;
   const signature = await sign(algorithm, merkleRoot, keyOrSigner);
   const proof: V4SignedDocument["proof"] = {
     ...document.proof,
-    key: SigningKey.guard(keyOrSigner) ? keyOrSigner.public : `did:ethr:${await keyOrSigner.getAddress()}#controller`,
+    key:
+      keyOrSigner instanceof ethers.Signer
+        ? `did:ethr:${await keyOrSigner.getAddress()}#controller`
+        : keyOrSigner.public,
     signature,
   };
   return { ...document, proof };
