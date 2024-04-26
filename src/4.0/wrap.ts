946ae6325ed281a6f8635b68e458280608f2b8be
Phan Shi Yu <phan_shi_yu@hive.gov.sg>
2024-04-26 12:54:05 +0800

    fix: was using the wrong document for the check

--- src/4.0/wrap.ts
+++ src/4.0/wrap.ts
@@ -9,40 +9,40 @@ import { interpretContexts } from "./validate";
 export const wrapDocument = async <T extends V4Document>(document: T): Promise<V4WrappedDocument<T>> => {
   /* 1a. try OpenAttestation VC validation, since most user will be issuing oa v4*/
   const oav4context = await V4Document.pick({ "@context": true }).safeParseAsync(document); // Superficial check on user intention
-  let rawDocument: W3cVerifiableCredential | undefined;
+  let validatedRawDocument: W3cVerifiableCredential | undefined;
   if (oav4context.success) {
-    const oav4 = await V4Document.safeParseAsync(rawDocument);
+    const oav4 = await V4Document.safeParseAsync(document);
     if (!oav4.success) {
       throw new Error(
         `Input document does not conform to OpenAttestation v4.0 Data Model: ${JSON.stringify(oav4.error.issues)}`
       );
     }
-    rawDocument = oav4.data;
+    validatedRawDocument = oav4.data;
   }
 
   /* 1b. only if OA VC validation fail do we continue with W3C VC data model validation */
-  if (!rawDocument) {
+  if (!validatedRawDocument) {
     const vc = await W3cVerifiableCredential.safeParseAsync(document);
     if (!vc.success)
       throw new Error(
         `Input document does not conform to Verifiable Credentials v2.0 Data Model: ${JSON.stringify(vc.error.issues)}`
       );
 
-    rawDocument = vc.data;
+    validatedRawDocument = vc.data;
   }
 
   /* 2. Ensure provided @context are interpretable (e.g. valid @context URL, all types are mapped, etc.) */
-  await interpretContexts(rawDocument);
+  await interpretContexts(validatedRawDocument);
 
   /* 3. Context validation */
   // Ensure that required contexts are present and in the correct order
   // type: [Base, OA, ...]
   const REQUIRED_CONTEXTS = [ContextUrl.v2_vc, ContextUrl.v4_alpha] as const;
   const contexts = new Set<string>(REQUIRED_CONTEXTS);
-  if (typeof rawDocument["@context"] === "string") {
-    contexts.add(rawDocument["@context"]);
-  } else if (isStringArray(rawDocument["@context"])) {
-    rawDocument["@context"].forEach((context) => contexts.add(context));
+  if (typeof validatedRawDocument["@context"] === "string") {
+    contexts.add(validatedRawDocument["@context"]);
+  } else if (isStringArray(validatedRawDocument["@context"])) {
+    validatedRawDocument["@context"].forEach((context) => contexts.add(context));
   }
   REQUIRED_CONTEXTS.forEach((c) => contexts.delete(c));
   const finalContexts: V4Document["@context"] = [...REQUIRED_CONTEXTS, ...Array.from(contexts)];
@@ -52,17 +52,17 @@ export const wrapDocument = async <T extends V4Document>(document: T): Promise<V
   // type: ["VerifiableCredential", "OpenAttestationCredential", ...]
   const REQUIRED_TYPES = [ContextType.BaseContext, ContextType.V4AlphaContext] as const;
   const types = new Set<string>([ContextType.BaseContext, ContextType.V4AlphaContext]);
-  if (typeof rawDocument["type"] === "string") {
-    types.add(rawDocument["type"]);
-  } else if (isStringArray(rawDocument["type"])) {
+  if (typeof validatedRawDocument["type"] === "string") {
+    types.add(validatedRawDocument["type"]);
+  } else if (isStringArray(validatedRawDocument["type"])) {
     types.forEach((type) => types.add(type));
   }
   REQUIRED_TYPES.forEach((t) => types.delete(t));
   const finalTypes: V4Document["type"] = [...REQUIRED_TYPES, ...Array.from(types)];
 
   const documentReadyForWrapping = {
-    ...rawDocument,
-    ...extractAndAssertAsV4DocumentProps(rawDocument, ["issuer", "credentialStatus"]),
+    ...validatedRawDocument,
+    ...extractAndAssertAsV4DocumentProps(validatedRawDocument, ["issuer", "credentialStatus"]),
     "@context": finalContexts,
     type: finalTypes,
   } satisfies W3cVerifiableCredential;
