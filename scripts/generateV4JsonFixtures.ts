dc614d070093627f8fadc9a920d4b5e9fd6f1243
Phan Shi Yu <phan_shi_yu@hive.gov.sg>
2024-04-26 21:37:41 +0800

    chore: auto generate v4 fixtures

---
+++ scripts/generateV4JsonFixtures.ts
@@ -0,0 +1,18 @@
+import fs from "fs";
+import path from "path";
+import * as V4_FIXTURES from "../src/4.0/fixtures";
+
+const OUTPUT_DIR = path.resolve("./test/fixtures/v4/__generated__");
+
+// make sure the output directory exists
+if (fs.existsSync(OUTPUT_DIR)) {
+  fs.rmSync(OUTPUT_DIR, { recursive: true });
+}
+fs.mkdirSync(OUTPUT_DIR);
+
+for (const [key, value] of Object.entries(V4_FIXTURES)) {
+  fs.writeFileSync(
+    path.join(OUTPUT_DIR, key.replace(/_/g, "-").toLowerCase() + ".json"),
+    JSON.stringify(value, null, 2)
+  );
+}
