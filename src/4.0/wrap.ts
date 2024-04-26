6245038de0c2628a2d1686d5ba63486f20a8b5bc
Phan Shi Yu <phan_shi_yu@hive.gov.sg>
2024-04-26 16:44:55 +0800

    fix: add back the missing wrapDocuments that was accidentally removed

--- src/4.0/wrap.ts
+++ src/4.0/wrap.ts
@@ -94,6 +94,32 @@ export const wrapDocument = async <T extends V4Document>(document: T): Promise<V
   return verifiableCredential as V4WrappedDocument<T>;
 };
 
+export const wrapDocuments = async <T extends V4Document>(documents: T[]): Promise<V4WrappedDocument<T>[]> => {
+  // create individual verifiable credential
+  const verifiableCredentials = await Promise.all(documents.map((document) => wrapDocument(document)));
+
+  // get all the target hashes to compute the merkle tree and the merkle root
+  const merkleTree = new MerkleTree(
+    verifiableCredentials.map((verifiableCredential) => verifiableCredential.proof.targetHash).map(hashToBuffer)
+  );
+  const merkleRoot = merkleTree.getRoot().toString("hex");
+
+  // for each document, update the merkle root and add the proofs needed
+  return verifiableCredentials.map((verifiableCredential) => {
+    const digest = verifiableCredential.proof.targetHash;
+    const merkleProof = merkleTree.getProof(hashToBuffer(digest)).map((buffer: Buffer) => buffer.toString("hex"));
+
+    return {
+      ...verifiableCredential,
+      proof: {
+        ...verifiableCredential.proof,
+        proofs: merkleProof,
+        merkleRoot,
+      },
+    };
+  });
+};
+
 /** Extract a set of properties from w3cVerifiableCredential but only include the ones
  * that are defined in the original document. For example, if we extract
  * "a" and "b" from { b: "something" } we should only get { b: "something" } NOT
