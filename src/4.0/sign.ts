5452590844ba1bce43a2c12faa68a5b2af35ebde
Phan Shi Yu <phan_shi_yu@hive.gov.sg>
2024-04-27 11:01:28 +0800

    fix: allow resigning and validate proof before using

--- src/4.0/sign.ts
+++ src/4.0/sign.ts
@@ -1,6 +1,5 @@
 import { sign } from "../shared/signer";
 import { SigningKey, SUPPORTED_SIGNING_ALGORITHM } from "../shared/@types/sign";
-import { isSignedWrappedV4Document } from "../shared/utils";
 import { ethers } from "ethers";
 import { V4Document, V4WrappedDocument, V4SignedWrappedDocument } from "./types";
 
@@ -9,11 +8,15 @@ export const signDocument = async <T extends V4Document>(
   algorithm: SUPPORTED_SIGNING_ALGORITHM,
   keyOrSigner: SigningKey | ethers.Signer
 ): Promise<V4SignedWrappedDocument<T>> => {
-  if (isSignedWrappedV4Document(document)) throw new Error("Document has been signed");
-  const merkleRoot = `0x${document.proof.merkleRoot}`;
+  const parsedResults = V4WrappedDocument.pick({ proof: true }).passthrough().safeParse(document);
+  if (!parsedResults.success) {
+    throw new Error("Document has not been wrapped yet");
+  }
+  const { proof: validatedProof } = parsedResults.data;
+  const merkleRoot = `0x${validatedProof.merkleRoot}`;
   const signature = await sign(algorithm, merkleRoot, keyOrSigner);
   const proof: V4SignedWrappedDocument["proof"] = {
-    ...document.proof,
+    ...validatedProof,
     key: SigningKey.guard(keyOrSigner) ? keyOrSigner.public : `did:ethr:${await keyOrSigner.getAddress()}#controller`,
     signature,
   };
