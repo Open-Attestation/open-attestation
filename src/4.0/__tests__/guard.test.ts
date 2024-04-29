defbb3978c895a20704db1e9968c1bd36dfdbfea
Phan Shi Yu <phan_shi_yu@hive.gov.sg>
2024-04-29 13:58:22 +0800

    chore: improve guard test to use direct output of our fns as test case

--- src/4.0/__tests__/guard.test.ts
+++ src/4.0/__tests__/guard.test.ts
@@ -1,7 +1,35 @@
+import { SUPPORTED_SIGNING_ALGORITHM } from "../../shared/@types/sign";
+import { RAW_DOCUMENT_DID } from "../fixtures";
+import { signDocument } from "../sign";
 import { W3cVerifiableCredential, V4Document, V4WrappedDocument, V4SignedWrappedDocument } from "../types";
-import { RAW_DOCUMENT_DID, SIGNED_WRAPPED_DOCUMENT_DID, WRAPPED_DOCUMENT_DID } from "../fixtures";
+import { wrapDocument } from "../wrap";
+
+const RAW_DOCUMENT = {
+  ...RAW_DOCUMENT_DID,
+  attachments: [
+    {
+      mimeType: "image/png",
+      fileName: "aaa",
+      data: "abcd",
+    },
+  ],
+} satisfies V4Document;
 
 describe("V4.0 guard", () => {
+  let WRAPPED_DOCUMENT: V4WrappedDocument;
+  let SIGNED_WRAPPED_DOCUMENT: V4SignedWrappedDocument;
+  beforeAll(async () => {
+    WRAPPED_DOCUMENT = await wrapDocument(RAW_DOCUMENT);
+    SIGNED_WRAPPED_DOCUMENT = await signDocument(
+      WRAPPED_DOCUMENT,
+      SUPPORTED_SIGNING_ALGORITHM.Secp256k1VerificationKey2018,
+      {
+        public: "did:ethr:0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89#controller",
+        private: "0x497c85ed89f1874ba37532d1e33519aba15bd533cdcb90774cc497bfe3cde655",
+      }
+    );
+  });
+
   describe("given a raw document", () => {
     test("should pass w3c vc validation without removal of any data", () => {
       const w3cVerifiableCredential: W3cVerifiableCredential = RAW_DOCUMENT_DID;
@@ -53,24 +81,24 @@ describe("V4.0 guard", () => {
 
   describe("given a wrapped document", () => {
     test("should pass w3c vc validation without removal of any data", () => {
-      const w3cVerifiableCredential: W3cVerifiableCredential = WRAPPED_DOCUMENT_DID;
+      const w3cVerifiableCredential: W3cVerifiableCredential = WRAPPED_DOCUMENT;
       const results = W3cVerifiableCredential.parse(w3cVerifiableCredential);
-      expect(results).toEqual(WRAPPED_DOCUMENT_DID);
+      expect(results).toEqual(WRAPPED_DOCUMENT);
     });
 
     test("should pass document validation without removal of any data", () => {
-      const v4Document: V4Document = WRAPPED_DOCUMENT_DID;
+      const v4Document: V4Document = WRAPPED_DOCUMENT;
       const results = V4Document.parse(v4Document);
-      expect(results).toEqual(WRAPPED_DOCUMENT_DID);
+      expect(results).toEqual(WRAPPED_DOCUMENT);
     });
 
     test("should pass wrapped document validation without removal of any data", () => {
-      const results = V4WrappedDocument.parse(WRAPPED_DOCUMENT_DID);
-      expect(results).toEqual(WRAPPED_DOCUMENT_DID);
+      const results = V4WrappedDocument.parse(WRAPPED_DOCUMENT);
+      expect(results).toEqual(WRAPPED_DOCUMENT);
     });
 
     test("should fail signed wrapped document validation", () => {
-      const results = V4SignedWrappedDocument.safeParse(WRAPPED_DOCUMENT_DID);
+      const results = V4SignedWrappedDocument.safeParse(WRAPPED_DOCUMENT);
       expect(results.success).toBe(false);
       expect((results as { error: unknown }).error).toMatchInlineSnapshot(`
         [ZodError: [
@@ -101,26 +129,26 @@ describe("V4.0 guard", () => {
 
   describe("given a signed wrapped document", () => {
     test("should pass w3c vc validation without removal of any data", () => {
-      const w3cVerifiableCredential: W3cVerifiableCredential = SIGNED_WRAPPED_DOCUMENT_DID;
+      const w3cVerifiableCredential: W3cVerifiableCredential = SIGNED_WRAPPED_DOCUMENT;
       const results = W3cVerifiableCredential.parse(w3cVerifiableCredential);
-      expect(results).toEqual(SIGNED_WRAPPED_DOCUMENT_DID);
+      expect(results).toEqual(SIGNED_WRAPPED_DOCUMENT);
     });
 
     test("should pass document validation without removal of any data", () => {
-      const v4Document: V4Document = SIGNED_WRAPPED_DOCUMENT_DID;
+      const v4Document: V4Document = SIGNED_WRAPPED_DOCUMENT;
       const results = V4Document.parse(v4Document);
-      expect(results).toEqual(SIGNED_WRAPPED_DOCUMENT_DID);
+      expect(results).toEqual(SIGNED_WRAPPED_DOCUMENT);
     });
 
     test("should pass wrapped document validation without removal of any data", () => {
-      const v4WrappedDocument: V4WrappedDocument = SIGNED_WRAPPED_DOCUMENT_DID;
+      const v4WrappedDocument: V4WrappedDocument = SIGNED_WRAPPED_DOCUMENT;
       const results = V4WrappedDocument.parse(v4WrappedDocument);
-      expect(results).toEqual(SIGNED_WRAPPED_DOCUMENT_DID);
+      expect(results).toEqual(SIGNED_WRAPPED_DOCUMENT);
     });
 
     test("should pass signed wrapped document validation without removal of any data", () => {
-      const results = V4SignedWrappedDocument.parse(SIGNED_WRAPPED_DOCUMENT_DID);
-      expect(results).toEqual(SIGNED_WRAPPED_DOCUMENT_DID);
+      const results = V4SignedWrappedDocument.parse(SIGNED_WRAPPED_DOCUMENT);
+      expect(results).toEqual(SIGNED_WRAPPED_DOCUMENT);
     });
   });
 });
