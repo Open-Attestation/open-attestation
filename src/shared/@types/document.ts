b7422f2547c724ed727b0f9f728e2c7b210a6b06
Phan Shi Yu <phan_shi_yu@hive.gov.sg>
2024-05-07 16:10:53 +0800

    fix: bump target platform, and remove baseUrl as it is not supported (#282)
    * fix: bump target platform, and remove baseUrl as it is not supported
    
    * refactor: lint

--- src/shared/@types/document.ts
+++ src/shared/@types/document.ts
@@ -11,7 +11,7 @@ import {
 } from "../../3.0/types";
 import { Literal, Static, String } from "runtypes";
 import { ethers } from "ethers";
-import { V4Document, V4SignedWrappedDocument, V4WrappedDocument } from "src/4.0/types";
+import { V4Document, V4SignedWrappedDocument, V4WrappedDocument } from "../../4.0/types";
 
 export type OpenAttestationDocument = OpenAttestationDocumentV2 | OpenAttestationDocumentV3 | V4Document;
 export type WrappedDocument<T extends OpenAttestationDocument> = T extends OpenAttestationDocumentV2
