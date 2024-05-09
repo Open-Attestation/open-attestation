6b470597f854d68dde8fbced436f44efda5600bf
Phan Shi Yu <phan_shi_yu@hive.gov.sg>
2024-05-09 08:10:32 +0800

    tests: added tests for revocation store revocation method

--- src/4.0/__tests__/documentBuilder.test.ts
+++ src/4.0/__tests__/documentBuilder.test.ts
@@ -287,6 +287,40 @@ describe(`DocumentBuilder`, () => {
     `);
   });
 
+  test("given revocation store revocation is added, should be added into credential status of the document", async () => {
+    const signed = await new DocumentBuilder({
+      content: { name: "John Doe" },
+      name: "Diploma",
+      attachments: [
+        {
+          data: "data",
+          fileName: "file",
+          mimeType: "application/pdf",
+        },
+      ],
+    })
+      .embeddedRenderer({
+        rendererUrl: "https://example.com",
+        templateName: "example",
+      })
+      .revocationStoreRevocation({
+        storeAddress: "0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89",
+      })
+      .dnsTxtIssuance({
+        identityProofDomain: "example.com",
+        issuerId: "did:example:123",
+        issuerName: "Example University",
+      })
+      .wrapAndSign({ signer: SAMPLE_SIGNING_KEYS });
+
+    expect(signed.credentialStatus).toMatchInlineSnapshot(`
+      {
+        "id": "0xE712878f6E8d5d4F9e87E10DA604F9cB564C9a89",
+        "type": "OpenAttestationRevocationStore",
+      }
+    `);
+  });
+
   test("given wrap only is called, should be able to sign the wrapped document with the standalone sign fn", async () => {
     const wrapped = await new DocumentBuilder({
       content: { name: "John Doe" },
@@ -328,14 +362,19 @@ describe(`DocumentBuilder`, () => {
       })
     ).toThrowError(DocumentBuilderErrors.ShouldNotModifyAfterSettingError);
 
-    documentWithRenderMethod.noRevocation().dnsTxtIssuance({
+    const documentWithNoRevocation = documentWithRenderMethod.noRevocation();
+    expect(() => documentWithRenderMethod.oscpRevocation({ oscpUrl: "https://oscp.example.com" })).toThrowError(
+      DocumentBuilderErrors.ShouldNotModifyAfterSettingError
+    );
+
+    documentWithNoRevocation.dnsTxtIssuance({
       identityProofDomain: "example.com",
       issuerId: "did:example:123",
       issuerName: "Example University",
     });
 
     expect(() =>
-      documentWithRenderMethod.noRevocation().dnsTxtIssuance({
+      documentWithNoRevocation.dnsTxtIssuance({
         identityProofDomain: "another.com",
         issuerId: "did:example:123",
         issuerName: "Example University",
@@ -370,14 +409,16 @@ describe(`DocumentBuilder`, () => {
       const builder = new DocumentBuilder({
         content: { name: "John Doe" },
         name: "Diploma",
-      }).embeddedRenderer({
-        rendererUrl: "https://example.com",
-        templateName: "example",
-      });
+      })
+        .embeddedRenderer({
+          rendererUrl: "https://example.com",
+          templateName: "example",
+        })
+        .noRevocation();
       let error;
       expect(() => {
         try {
-          builder.noRevocation().dnsTxtIssuance({
+          builder.dnsTxtIssuance({
             identityProofDomain: "example.com",
             issuerId: "invalid",
             issuerName: "Example University",
@@ -387,16 +428,49 @@ describe(`DocumentBuilder`, () => {
           throw e;
         }
       }).toThrowErrorMatchingInlineSnapshot(`
-      "Invalid props: 
-       {
-        "_errors": [],
-        "issuerId": {
-          "_errors": [
-            "Invalid URI"
-          ]
+              "Invalid props: 
+               {
+                "_errors": [],
+                "issuerId": {
+                  "_errors": [
+                    "Invalid URI"
+                  ]
+                }
+              }"
+          `);
+      expect(error).toBeInstanceOf(DocumentBuilderErrors.PropsValidationError);
+    });
+
+    test("given an invalid ethereum address for revocation store, should throw", () => {
+      const builder = new DocumentBuilder({
+        content: { name: "John Doe" },
+        name: "Diploma",
+      }).embeddedRenderer({
+        rendererUrl: "https://example.com",
+        templateName: "example",
+      });
+
+      let error;
+      expect(() => {
+        try {
+          builder.revocationStoreRevocation({
+            storeAddress: "0x123",
+          });
+        } catch (e) {
+          error = e;
+          throw e;
         }
-      }"
-    `);
+      }).toThrowErrorMatchingInlineSnapshot(`
+        "Invalid props: 
+         {
+          "_errors": [],
+          "storeAddress": {
+            "_errors": [
+              "Invalid Ethereum address"
+            ]
+          }
+        }"
+      `);
       expect(error).toBeInstanceOf(DocumentBuilderErrors.PropsValidationError);
     });
   });
