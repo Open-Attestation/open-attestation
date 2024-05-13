69bd41a0cb0669d4403539fec44422d56da01781
Phan Shi Yu <phan_shi_yu@hive.gov.sg>
2024-05-13 18:24:48 +0800

    fix: obfuscation return type should still have a compliant attachment… (#293)
    * fix: obfuscation return type should still have a compliant attachment array
    
    * fix: improve typing more

--- src/4.0/obfuscate.ts
+++ src/4.0/obfuscate.ts
@@ -68,7 +68,16 @@ const obfuscate = (_data: V4WrappedDocument, fields: string[] | string) => {
 
 export type ObfuscateVerifiableCredentialResult<T extends V4WrappedDocument> = Override<
   T,
-  { credentialSubject: PartialDeep<T["credentialSubject"]> }
+  {
+    credentialSubject: Override<
+      PartialDeep<T["credentialSubject"]>,
+      {
+        attachments?: T["credentialSubject"]["attachments"] extends Array<unknown>
+          ? T["credentialSubject"]["attachments"]
+          : undefined;
+      }
+    >;
+  }
 >;
 export const obfuscateVerifiableCredential = <T extends V4WrappedDocument | V4SignedWrappedDocument>(
   document: T,
