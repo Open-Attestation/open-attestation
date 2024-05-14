20f2f6697e8483e2e4e015e8399be7438ddccd5e
Kyle Huang Junyuan <kyle@hjunyuan.com>
2024-05-14 17:25:14 +0800

    fix: mark v3 functions as deprecated (#298)

--- src/shared/@types/wrap.ts
+++ src/shared/@types/wrap.ts
@@ -4,10 +4,15 @@ export interface WrapDocumentOption {
   externalSchemaId?: string;
   version?: SchemaId;
 }
+
 export interface WrapDocumentOptionV2 {
   externalSchemaId?: string;
   version?: SchemaId.v2;
 }
+
+/**
+ * @deprecated will be removed in the next major release in favour of OpenAttestation v4.0 (more info: https://github.com/Open-Attestation/open-attestation/tree/alpha)
+ */
 export interface WrapDocumentOptionV3 {
   externalSchemaId?: string;
   version: SchemaId.v3;
