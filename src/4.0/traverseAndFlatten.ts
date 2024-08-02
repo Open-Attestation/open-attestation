51d910ee2dd6a5ab780a334563674fb9ff35e0f6
Phan Shi Yu <phan_shi_yu@hive.gov.sg>
2024-08-02 17:38:47 +0800

    fix: obfuscation of an array element can lead to undefined error (#312)
    * fix: obfuscation of an array element can lead to undefined error
    
    * fix: typo
    
    ---------
    
    Co-authored-by: Phan Shi Yu <phan_shi_yu@tech.gov.sg>

--- src/4.0/traverseAndFlatten.ts
+++ src/4.0/traverseAndFlatten.ts
@@ -1,9 +1,13 @@
 export type LeafValue = string | number | boolean | null | Record<string, never> | [];
 
+// we use a symbol just in case of conflicts with what the iteratee returns
+const undefinedSym = Symbol("undefined value that should be filtered away");
+
 function _traverseAndFlatten<IterateeValue>(
   data: unknown,
   iteratee: (data: { value: LeafValue; path: string }) => IterateeValue,
-  path = ""
+  path = "",
+  isRemoveUndefinedLeafNodes = false
 ): IterateeValue | IterateeValue[] {
   if (Array.isArray(data)) {
     // an empty array is considered a leaf node
@@ -13,7 +17,7 @@ function _traverseAndFlatten<IterateeValue>(
     const results: IterateeValue[] = [];
     for (let index = 0; index < data.length; index++) {
       const value = data[index];
-      const result = _traverseAndFlatten(value, iteratee, `${path}[${index}]`);
+      const result = _traverseAndFlatten(value, iteratee, `${path}[${index}]`, isRemoveUndefinedLeafNodes);
       if (Array.isArray(result)) {
         results.push(...result);
       } else {
@@ -29,12 +33,22 @@ function _traverseAndFlatten<IterateeValue>(
     // an empty object is considered a leaf node
     if (keys.length === 0) return iteratee({ value: {}, path });
     return Object.keys(data).flatMap((key) =>
-      _traverseAndFlatten(data[key as keyof typeof data], iteratee, path ? `${path}.${key}` : key)
+      _traverseAndFlatten(
+        data[key as keyof typeof data],
+        iteratee,
+        path ? `${path}.${key}` : key,
+        isRemoveUndefinedLeafNodes
+      )
     );
   }
 
   if (typeof data === "string" || typeof data === "number" || typeof data === "boolean" || data === null) {
     return iteratee({ value: data, path });
+  }
+
+  if (data === undefined && isRemoveUndefinedLeafNodes) {
+    // mark undefined leaf node with a unique symbol that must be removed later
+    return undefinedSym as IterateeValue;
   }
 
   throw new Error(`Unexpected data '${data}' in '${path}'`);
@@ -44,8 +58,11 @@ function _traverseAndFlatten<IterateeValue>(
 export function traverseAndFlatten<IterateeValue>(
   data: Record<string, unknown>,
   iteratee: (data: { value: LeafValue; path: string }) => IterateeValue,
-  path = ""
+  path = "",
+  /** by default throw when there are leaf nodes that are undefined, setting this to true will remove them instead */
+  isRemoveUndefinedLeafNodes = false
 ): IterateeValue[] {
-  const results = _traverseAndFlatten(data, iteratee, path);
-  return Array.isArray(results) ? results : [];
+  const results = _traverseAndFlatten(data, iteratee, path, isRemoveUndefinedLeafNodes);
+  // remove undefined leaf nodes
+  return Array.isArray(results) ? results.filter((v) => v !== undefinedSym) : [];
 }
